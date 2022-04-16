6547a7dbb8549fe6e845755ada9a423dcdb38c2e
axing <1253740807@qq.com>
2022-04-16 14:28:48 +0800

    add 服务器

---
+++ Cluster_2/Master/modoverrides.lua
@@ -0,0 +1,70 @@
+return {
+  ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
+  ["workshop-1530801499"]={
+    configuration_options={ Hunger_Cost=0, Ownership=false, Sanity_Cost=0 },
+    enabled=true 
+  },
+  ["workshop-1860955902"]={ configuration_options={  }, enabled=true },
+  ["workshop-1898181913"]={ configuration_options={  }, enabled=true },
+  ["workshop-2527328931"]={
+    configuration_options={
+      Animal=true,
+      BuildRepair=true,
+      ChopTime=999,
+      CookTime=998,
+      DryTime=999,
+      FishTime=999,
+      HSHU=true,
+      MineTime=999,
+      Others=true,
+      Pick=true,
+      Plant=true 
+    },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-462434129"]={
+    configuration_options={
+      MOD_RESTART_ALLOW_KILL=true,
+      MOD_RESTART_ALLOW_RESTART=true,
+      MOD_RESTART_ALLOW_RESURRECT=true,
+      MOD_RESTART_CD_BONUS=0,
+      MOD_RESTART_CD_KILL=1,
+      MOD_RESTART_CD_MAX=0,
+      MOD_RESTART_CD_RESTART=1,
+      MOD_RESTART_CD_RESURRECT=1,
+      MOD_RESTART_FORCE_DROP_MODE=0,
+      MOD_RESTART_IGNORING_ADMIN=true,
+      MOD_RESTART_MAP_SAVE=2,
+      MOD_RESTART_RESURRECT_HEALTH=0,
+      MOD_RESTART_TRIGGER_MODE=1,
+      MOD_RESTART_WELCOME_TIPS=true,
+      MOD_RESTART_WELCOME_TIPS_TIME=8 
+    },
+    enabled=true 
+  },
+  ["workshop-661253977"]={
+    configuration_options={ amudiao=true, baodiao=1, kong=0, nillots=0, rendiao=2, zbdiao=true },
+    enabled=true 
+  },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      display_hp=-1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  },
+  ["workshop-786556008"]={
+    configuration_options={ ENABLEBACKPACK=false, EXTRASLOT=0, INVENTORYSIZE=45 },
+    enabled=true 
+  } 
+}
